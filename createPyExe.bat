6659b277a46222e2f6e9ff979d9cb67ed9e313db
Wolfmyths <tdellegrin@gmail.com>
2024-04-14 21:00:26 -0400

    2.1

--- createPyExe.bat
+++ createPyExe.bat
@@ -1,7 +1,7 @@
 @echo on
 
 echo Setting Variables
-set disFolder="Genshin Stopwatch"
+set disFolder="./Genshin Stopwatch"
 set Txt="requirements.txt"
 set Spec="./src/python/main.spec"
 
@@ -9,6 +9,6 @@ echo Installing Dependencies
 pip install -r %Txt%
 
 echo Running Pyinstaller
-pyinstaller --clean %Spec% --distpath ./%disFolder%
+pyinstaller --clean %Spec% --distpath %disFolder%
 
 echo Installation Finished!
